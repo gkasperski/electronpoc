00fcd04617d2830276dc498da4f08d4198839374
Grzegorz Kasperski <grzegorz.kasperski@globallogic.com>
2018-06-05 15:05:13 +0200

    merge

--- app/plugins/simplePlugin.js
+++
@@ -1,9 +0,0 @@
-exports.init = api => ({
-  run() {
-    api.wyswietlTekst('testowy');
-
-    api.event.on('scanning', data => {
-      console.log(`plugin see scanning and data: ${data}`);
-    });
-  }
-});
